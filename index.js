fd0fde3ce55bac37f7dba66ad59e00f75ec8c65c
Nazarov8201 <nazarov.javlon@internet.ru>
2023-03-17 20:17:26 +0500

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var swiper = new Swiper(".slider-block", {
       spaceBetween: 30,
       centeredSlides: true,
+      loop: true,
       autoplay: {
         delay: 2500,
         disableOnInteraction: false,
@@ -18,6 +19,41 @@ var swiper = new Swiper(".slider-block", {
     var swiper = new Swiper(".slider-about", {
       spaceBetween: 30,
       centeredSlides: true,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+    var swiper = new Swiper(".slider-latest", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+    var swiper = new Swiper(".slider-gallery", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      loop: true,
       autoplay: {
         delay: 2500,
         disableOnInteraction: false,
