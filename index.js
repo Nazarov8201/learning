376bca422a2f0667f0447e5f4437545a1bbf1e14
Nazarov8201 <nazarov.javlon@internet.ru>
2023-03-29 21:44:07 +0500

    first commit

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ var swiper = new Swiper(".slider-block", {
 
     var swiper = new Swiper(".slider-about", {
       spaceBetween: 30,
-      centeredSlides: true,
       loop: true,
       autoplay: {
         delay: 2500,
@@ -31,11 +30,24 @@ var swiper = new Swiper(".slider-block", {
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+      },
+      centeredSlides: false,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 0,
+          slidesPerGroup: 2,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 0,
+          slidesPerGroup: 4,
+        },
       },
     });
     var swiper = new Swiper(".slider-latest", {
+      slidesPerView: 1,
       spaceBetween: 30,
-      centeredSlides: true,
       loop: true,
       autoplay: {
         delay: 2500,
@@ -48,11 +60,24 @@ var swiper = new Swiper(".slider-block", {
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+      },
+      centeredSlides: false,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+          slidesPerGroup: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+          slidesPerGroup: 3,
+        },
       },
     });
     var swiper = new Swiper(".slider-gallery", {
+      slidesPerView: 1,
       spaceBetween: 30,
-      centeredSlides: true,
       loop: true,
       autoplay: {
         delay: 2500,
@@ -65,5 +90,78 @@ var swiper = new Swiper(".slider-block", {
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+      },
+      centeredSlides: false,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 0,
+          slidesPerGroup: 2,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 0,
+          slidesPerGroup: 4,
+        },
+      },
+    });
+    var swiper = new Swiper(".slider-tea", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      centeredSlides: false,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 0,
+          slidesPerGroup: 2,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 0,
+          slidesPerGroup: 4,
+        },
+      },
+    });
+    var swiper = new Swiper(".slider-our", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      centeredSlides: false,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+          slidesPerGroup: 2,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+          slidesPerGroup: 4,
+        },
       },
     });
